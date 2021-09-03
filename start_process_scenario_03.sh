f5bbcd724374c28b53ca672321e56af9e232a8d9
andre-mare <andre.mare@gmail.com>
2021-09-03 17:09:14 +0200

    Change REST Endpoints

--- start_process_scenario_03.sh
+++ start_process_scenario_03.sh
@@ -1,4 +1,4 @@
-curl --location --request POST 'http://localhost:8080/rest/process-definition/key/place-order-process/start' --header 'Content-Type: application/json' --data-raw '{
+curl --location --request POST 'http://localhost:8080/engine-rest/process-definition/key/place-order-process/start' --header 'Content-Type: application/json' --data-raw '{
   "variables": {
     "orderedCoffee": {
       "value": "true",
